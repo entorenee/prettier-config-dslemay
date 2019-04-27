799a0bee95345178af0fbc644c5c13a65461e3e1
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-04-27 11:04:35 -0700

    feat: create Prettier config for package

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
